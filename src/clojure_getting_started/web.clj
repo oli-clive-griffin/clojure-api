7ca7b0ab2d534c77277d0407c772020cda27c215
Oli <o.clivegriffin@gmail.com>
2022-03-11 21:13:37 +1300

    tutorial step someting

--- src/clojure_getting_started/web.clj
+++ src/clojure_getting_started/web.clj
@@ -4,7 +4,9 @@
             [compojure.route :as route]
             [clojure.java.io :as io]
             [ring.adapter.jetty :as jetty]
-            [environ.core :refer [env]]))
+            [environ.core :refer [env]]
+            [camel-snake-kebab.core :as kebab]))
+
 
 (defn splash []
   {:status 200
@@ -12,14 +14,26 @@
    :body "Hello from Heroku"})
 
 (defroutes app
+  (GET "/camel" {{input :input} :params}
+        {:status 200
+        :headers {"Content-Type" "text/plain"}
+        :body (kebab/->camelCase input)})
+  (GET "/snake" {{input :input} :params}
+        {:status 200
+        :headers {"Content-Type" "text/plain"}
+        :body (kebab/->snake_case input)})
+  (GET "/kebab" {{input :input} :params}
+        {:status 200
+        :headers {"Content-Type" "text/plain"}
+        :body (kebab/->kebab-case input)})
   (GET "/" []
-       (splash))
+        (splash))
   (ANY "*" []
-       (route/not-found (slurp (io/resource "404.html")))))
-
-(defn -main [& [port]]
-  (let [port (Integer. (or port (env :port) 5000))]
-    (jetty/run-jetty (site #'app) {:port port :join? false})))
+        (route/not-found (slurp (io/resource "404.html")))))
+    
+  (defn -main [& [port]]
+    (let [port (Integer. (or port (env :port) 5000))]
+      (jetty/run-jetty (site #'app) {:port port :join? false})))
 
 ;; For interactive development:
 ;; (.stop server)
